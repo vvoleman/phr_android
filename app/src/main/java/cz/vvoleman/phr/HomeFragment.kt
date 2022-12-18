8951d3a0594a8b38c7126d70534a4ee4bdfd7fa2
Vojtěch Voleman <vvoleman@seznam.cz>
2022-12-18 20:53:48 +0100

    Init base project

---
+++ app/src/main/java/cz/vvoleman/phr/HomeFragment.kt
@@ -0,0 +1,7 @@
+package cz.vvoleman.phr
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment(R.layout.fragment_home){
+    
+}
---
+++ app/src/main/java/cz/vvoleman/phr/LoginFragment.kt
@@ -0,0 +1,7 @@
+package cz.vvoleman.phr
+
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment(R.layout.fragment_login){
+
+}
