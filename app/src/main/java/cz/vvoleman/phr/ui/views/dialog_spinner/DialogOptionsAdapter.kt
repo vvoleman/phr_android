b92fca494779cb925e42fdb7c095834c7b59e24c
Vojtěch Voleman <vvoleman@seznam.cz>
2023-01-24 18:39:15 +0100

    BPTULPHR-3 Fix adapter

--- app/src/main/java/cz/vvoleman/phr/ui/views/dialog_spinner/DialogOptionsAdapter.kt
+++ app/src/main/java/cz/vvoleman/phr/ui/views/dialog_spinner/DialogOptionsAdapter.kt
@@ -1,16 +1,20 @@
 package cz.vvoleman.phr.ui.views.dialog_spinner
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import cz.vvoleman.phr.data.AdapterPair
 import cz.vvoleman.phr.databinding.CustomDialogSpinnerItemBinding
 
+private val TAG = "DialogOptionsAdapter"
+
 class DialogOptionsAdapter (
     private val listener: OnItemClickListener,
-) : ListAdapter<AdapterPair, DialogOptionsAdapter.DialogOptionsViewHolder>(DiffCallback()) {
+) : PagingDataAdapter<AdapterPair, DialogOptionsAdapter.DialogOptionsViewHolder>(DiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DialogOptionsViewHolder {
         val binding = CustomDialogSpinnerItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -19,7 +23,10 @@ class DialogOptionsAdapter (
 
     override fun onBindViewHolder(holder: DialogOptionsViewHolder, position: Int) {
         val currentItem = getItem(position)
-        holder.bind(currentItem)
+
+        if (currentItem != null) {
+            holder.bind(currentItem)
+        }
     }
 
     inner class DialogOptionsViewHolder(private val binding: CustomDialogSpinnerItemBinding) : RecyclerView.ViewHolder(binding.root) {
@@ -30,10 +37,13 @@ class DialogOptionsAdapter (
                     val position = bindingAdapterPosition
                     if (position != RecyclerView.NO_POSITION) {
                         val item = getItem(position)
-                        val shouldUncheck = listener.onItemClicked(item, position)
+                        if (item != null) {
+                            Log.d(TAG, "clicked: $item")
+                            val shouldUncheck = listener.onItemClicked(item, position)
 
-                        if (shouldUncheck) {
-                            radioButton.isChecked = false
+                            if (shouldUncheck) {
+                                radioButton.isChecked = false
+                            }
                         }
                     }
                 }
