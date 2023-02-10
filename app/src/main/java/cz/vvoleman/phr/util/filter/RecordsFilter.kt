cb94f399f12411a55e5c7de89e69bed3d64eb50b
Vojtěch Voleman <vojtech.voleman@tul.cz>
2023-02-10 21:03:03 +0100

    REBASE

--- app/src/main/java/cz/vvoleman/phr/util/filter/RecordsFilter.kt
+++ app/src/main/java/cz/vvoleman/phr/util/filter/RecordsFilter.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import cz.vvoleman.phr.data.PreferencesManager
 import cz.vvoleman.phr.data.medical_records.MedicalRecordWithDetails
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 
@@ -17,6 +18,8 @@ class RecordsFilter(
     val sortBy = preferences.preferencesFlow.map { userPreferences ->
         userPreferences.orderRecordsBy
     }
+
+    val selectedFacilities = MutableStateFlow<List<Int>>(emptyList())
 
     val facilities = allRecordsFlow
         .map { record ->
@@ -41,7 +44,19 @@ class RecordsFilter(
             Log.d(TAG, "diagnoses: ${it.size}")
         }
 
+    suspend fun changeSortBy(sortBy: String) {
+        preferences.setOrderRecordsBy(sortBy)
+    }
 
+    suspend fun toggleFacility(facilityId: Int) {
+        val facilities = selectedFacilities.value.toMutableList()
+        if (facilities.contains(facilityId)) {
+            facilities.remove(facilityId);
+        } else {
+            facilities.add(facilityId)
+        }
+        selectedFacilities.value = facilities
+    }
 
 
 }
