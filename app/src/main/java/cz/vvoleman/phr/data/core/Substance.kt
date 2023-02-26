757bba247dd3e34d114247d14be3303c16b42bed
Vojtěch Voleman <vojtech.voleman@tul.cz>
2023-02-26 23:53:56 +0100

    BPTULPHR-16 Add Medicine

---
+++ app/src/main/java/cz/vvoleman/phr/data/core/Substance.kt
@@ -0,0 +1,17 @@
+package cz.vvoleman.phr.data.core
+
+import android.os.Parcelable
+import cz.vvoleman.phr.data.AdapterPair
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Substance(
+    val id: String,
+    val name: String,
+    val isAddictive: Boolean,
+    val isDoping: Boolean,
+) : Parcelable, BaseData() {
+    override fun getAdapterPair(): AdapterPair {
+        return AdapterPair(id, name, this)
+    }
+}
