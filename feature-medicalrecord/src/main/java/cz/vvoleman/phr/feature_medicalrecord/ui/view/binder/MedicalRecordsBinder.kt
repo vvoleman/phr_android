b7d3a73b0664b2649fcbf08a53a8a10018e5de45
Vojtěch Voleman <vojtech.voleman@tul.cz>
2023-03-17 11:28:13 +0100

    Sync

--- feature-medicalrecord/src/main/java/cz/vvoleman/phr/feature_medicalrecord/ui/view/binder/MedicalRecordsBinder.kt
+++ feature-medicalrecord/src/main/java/cz/vvoleman/phr/feature_medicalrecord/ui/view/binder/MedicalRecordsBinder.kt
@@ -7,21 +7,27 @@ import com.google.android.material.sidesheet.SideSheetDialog
 import cz.vvoleman.phr.base.ui.mapper.ViewStateBinder
 import cz.vvoleman.phr.common.ui.adapter.filter.FilterAdapter
 import cz.vvoleman.phr.common.ui.adapter.grouped.GroupedItemsAdapter
+import cz.vvoleman.phr.common.ui.model.FilterPair
 import cz.vvoleman.phr.feature_medicalrecord.R
 import cz.vvoleman.phr.feature_medicalrecord.databinding.FragmentListMedicalRecordsBinding
 import cz.vvoleman.phr.feature_medicalrecord.databinding.SheetRecordsFilterBinding
 import cz.vvoleman.phr.feature_medicalrecord.presentation.list.model.ListMedicalRecordsViewState
 import cz.vvoleman.phr.feature_medicalrecord.ui.mapper.GroupedItemsDomainModelToUiMapper
 import cz.vvoleman.phr.feature_medicalrecord.ui.model.MedicalRecordUiModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 
 class MedicalRecordsBinder(
     private val groupedItemsDomainModelToUiMapper: GroupedItemsDomainModelToUiMapper
-): ViewStateBinder<ListMedicalRecordsViewState, FragmentListMedicalRecordsBinding> {
+): ViewStateBinder<ListMedicalRecordsViewState, FragmentListMedicalRecordsBinding>, FilterAdapter.FilterAdapterListener {
 
     private lateinit var adapter: GroupedItemsAdapter<MedicalRecordUiModel>
     private lateinit var filterSheet: SideSheetDialog
     private lateinit var problemCategoryAdapter: FilterAdapter
     private lateinit var medicalWorkerAdapter: FilterAdapter
+
+    private val _notification = MutableSharedFlow<Notification>()
+    val notification = _notification.asSharedFlow()
 
     override fun init(viewBinding: FragmentListMedicalRecordsBinding, context: Context) {
         val sideSheetBinding = SheetRecordsFilterBinding
@@ -33,7 +39,7 @@ class MedicalRecordsBinder(
             filterSheet.hide()
         }
 
-        problemCategoryAdapter = FilterAdapter(this)
+        //problemCategoryAdapter = FilterAdapter(this)
     }
 
     override fun bind(
@@ -56,6 +62,14 @@ class MedicalRecordsBinder(
         } else {
             filterSheet.show()
         }
+    }
+
+    sealed class Notification {
+        data class OptionCheckChanged(val item: FilterPair): Notification()
+    }
+
+    override fun onOptionCheckChanged(item: FilterPair) {
+        _notification.tryEmit(Notification.OptionCheckChanged(item))
     }
 
 }
