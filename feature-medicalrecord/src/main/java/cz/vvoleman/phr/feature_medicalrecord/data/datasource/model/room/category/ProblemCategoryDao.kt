4ac9fe513de718af350f9192e41a92fb02a17aa8
Vojtěch Voleman <vvoleman@seznam.cz>
2023-03-21 14:04:15 +0100

    sync

--- feature-medicalrecord/src/main/java/cz/vvoleman/phr/feature_medicalrecord/data/datasource/model/room/category/ProblemCategoryDao.kt
+++ feature-medicalrecord/src/main/java/cz/vvoleman/phr/feature_medicalrecord/data/datasource/model/room/category/ProblemCategoryDao.kt
@@ -14,7 +14,7 @@ interface ProblemCategoryDao {
     @Query("SELECT * FROM problem_category WHERE patient_id = :patientId")
     fun getByPatientId(patientId: Int): Flow<List<ProblemCategoryDataSourceModel>>
 
-    @Query("SELECT * FROM problem_category WHERE id IN (SELECT * FROM medical_record WHERE patient_id = :patientId)")
+    @Query("SELECT * FROM problem_category WHERE id IN (SELECT problem_category_id FROM medical_record WHERE patient_id = :patientId)")
     fun getUsedByPatientId(patientId: String): Flow<List<ProblemCategoryDataSourceModel>>
 
     @Query("SELECT * FROM problem_category WHERE id = :id")
