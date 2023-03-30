3dff9e65d6ea9e4e224831c707b7031869ccd316
Vojtěch Voleman <vojtech.voleman@tul.cz>
2023-03-30 12:09:59 +0200

    Add ListPatients design and switch

--- common/src/main/java/cz/vvoleman/phr/common/ui/model/PatientUiModel.kt
+++ common/src/main/java/cz/vvoleman/phr/common/ui/model/PatientUiModel.kt
@@ -5,5 +5,6 @@ import java.time.LocalDate
 data class PatientUiModel(
     val id: String,
     val name: String,
-    val birthDate: LocalDate? = null
+    val birthDate: LocalDate? = null,
+    val isSelected: Boolean = false
 )
