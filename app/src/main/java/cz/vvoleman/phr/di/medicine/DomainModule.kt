40baba4c24eab96909f014d5c008cb35557b6798
Vojtěch Voleman <vojtech.voleman@tul.cz>
2023-04-03 22:06:26 +0200

    Add base structure

---
+++ app/src/main/java/cz/vvoleman/phr/di/medicine/DomainModule.kt
@@ -0,0 +1,10 @@
+package cz.vvoleman.phr.di.medicine
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DomainModule {
+}
