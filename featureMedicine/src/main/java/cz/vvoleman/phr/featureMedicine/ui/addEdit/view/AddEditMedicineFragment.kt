fe38a27fdb43a16c9e05f9258273fe63f394513d
Vojtěch Voleman <vojtech.voleman@tul.cz>
2023-09-17 23:25:27 +0200

    BPTULPHR-49 Add base add, update and delete

--- featureMedicine/src/main/java/cz/vvoleman/phr/featureMedicine/ui/addEdit/view/AddEditMedicineFragment.kt
+++ featureMedicine/src/main/java/cz/vvoleman/phr/featureMedicine/ui/addEdit/view/AddEditMedicineFragment.kt
@@ -1,25 +1,36 @@
 package cz.vvoleman.phr.featureMedicine.ui.addEdit.view
 
+import android.app.TimePickerDialog
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TimePicker
+import androidx.appcompat.widget.PopupMenu
 import androidx.fragment.app.viewModels
 import com.google.android.material.snackbar.Snackbar
 import cz.vvoleman.phr.base.ui.mapper.ViewStateBinder
 import cz.vvoleman.phr.base.ui.view.BaseFragment
+import cz.vvoleman.phr.featureMedicine.R
 import cz.vvoleman.phr.featureMedicine.databinding.FragmentAddEditMedicineBinding
 import cz.vvoleman.phr.featureMedicine.presentation.addEdit.model.AddEditMedicineNotification
 import cz.vvoleman.phr.featureMedicine.presentation.addEdit.model.AddEditMedicineViewState
 import cz.vvoleman.phr.featureMedicine.presentation.addEdit.viewmodel.AddEditMedicineViewModel
+import cz.vvoleman.phr.featureMedicine.presentation.model.addEdit.TimePresentationModel
 import cz.vvoleman.phr.featureMedicine.ui.addEdit.mapper.AddEditMedicineDestinationUiMapper
+import cz.vvoleman.phr.featureMedicine.ui.mapper.addEdit.TimeUiModelToPresentationMapper
 import cz.vvoleman.phr.featureMedicine.ui.medicineSelector.MedicineSelector
 import cz.vvoleman.phr.featureMedicine.ui.model.list.MedicineUiModel
+import cz.vvoleman.phr.featureMedicine.ui.timeSelector.TimeSelector
+import cz.vvoleman.phr.featureMedicine.ui.timeSelector.TimeUiModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.time.LocalTime
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class AddEditMedicineFragment :
     BaseFragment<AddEditMedicineViewState, AddEditMedicineNotification, FragmentAddEditMedicineBinding>(),
-    MedicineSelector.MedicineSelectorListener {
+    MedicineSelector.MedicineSelectorListener,
+    TimeSelector.TimeSelectorListener {
 
     override val viewModel: AddEditMedicineViewModel by viewModels()
 
@@ -28,6 +39,9 @@ class AddEditMedicineFragment :
 
     @Inject
     override lateinit var viewStateBinder: ViewStateBinder<AddEditMedicineViewState, FragmentAddEditMedicineBinding>
+
+    @Inject
+    lateinit var timeUiModelToPresentationMapper: TimeUiModelToPresentationMapper
 
     override fun setupBinding(
         inflater: LayoutInflater,
@@ -40,6 +54,13 @@ class AddEditMedicineFragment :
         super.setupListeners()
 
         binding.medicineSelector.setListener(this)
+        binding.timeSelector.setListener(this)
+
+        binding.buttonAddTime.setOnClickListener {
+            openTimeDialog { time, _ ->
+                viewModel.onTimeAdd(time)
+            }
+        }
     }
 
     override fun handleNotification(notification: AddEditMedicineNotification) {
@@ -56,5 +77,52 @@ class AddEditMedicineFragment :
 
     override fun onMedicineSelectorSearch(query: String) {
         viewModel.onSearch(query)
+    }
+
+    override fun onTimeClick(index: Int, anchorView: View) {
+        val popup = PopupMenu(requireContext(), anchorView)
+        popup.inflate(R.menu.options_add_medicine_time)
+
+        popup.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.action_edit -> {
+                    openTimeDialog(index) { updatedTime, index ->
+                        viewModel.onTimeUpdate(index!!, updatedTime)
+                    }
+                    true
+                }
+
+                R.id.action_delete -> {
+                    viewModel.onTimeDelete(index)
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+        popup.show()
+    }
+
+    private fun openTimeDialog(index: Int? = null, updateUnit: (TimePresentationModel, Int?) -> Unit) {
+        val time = if (index != null) {
+            timeUiModelToPresentationMapper.toUi(viewModel.onGetTime(index))
+        } else {
+            TimeUiModel(LocalTime.now(), 1)
+        }
+
+        val dialog = TimePickerDialog(
+            context,
+            { _: TimePicker, selectedHour: Int, selectedMinute: Int ->
+                val updatedTime = time.copy(time = LocalTime.of(selectedHour, selectedMinute))
+
+                updateUnit(timeUiModelToPresentationMapper.toPresentation(updatedTime), index)
+            },
+            time.time.hour,
+            time.time.minute,
+            true
+        )
+
+        dialog.show()
     }
 }
