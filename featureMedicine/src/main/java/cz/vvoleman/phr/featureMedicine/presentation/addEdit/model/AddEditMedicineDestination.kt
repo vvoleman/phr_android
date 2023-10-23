5ed3bad80a507276cdc1065015ba3ba392ba28c0
Vojtěch Voleman <vojtech.voleman@tul.cz>
2023-10-23 17:59:44 +0200

    Add dynamic time selector item width

--- featureMedicine/src/main/java/cz/vvoleman/phr/featureMedicine/presentation/addEdit/model/AddEditMedicineDestination.kt
+++ featureMedicine/src/main/java/cz/vvoleman/phr/featureMedicine/presentation/addEdit/model/AddEditMedicineDestination.kt
@@ -2,4 +2,8 @@ package cz.vvoleman.phr.featureMedicine.presentation.addEdit.model
 
 import cz.vvoleman.phr.base.presentation.model.PresentationDestination
 
-sealed class AddEditMedicineDestination : PresentationDestination
+sealed class AddEditMedicineDestination : PresentationDestination {
+
+    data class MedicineSaved(val id: String) : AddEditMedicineDestination()
+
+}
