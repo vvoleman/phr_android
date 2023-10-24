a526b5f2b1121c9158a7a92f5922e75599682c39
Vojtěch Voleman <vojtech.voleman@tul.cz>
2023-10-24 13:17:24 +0200

    BPTULPHR-50 Prepare unit test for GetNextScheduledUseCase

---
+++ featureMedicine/src/main/java/cz/vvoleman/phr/featureMedicine/domain/repository/timeline/GetNextScheduledRepository.kt
@@ -0,0 +1,9 @@
+package cz.vvoleman.phr.featureMedicine.domain.repository.timeline
+
+import cz.vvoleman.phr.featureMedicine.domain.model.schedule.MedicineScheduleDomainModel
+
+interface GetNextScheduledRepository {
+
+    suspend fun getNextScheduled(patientId: String, limit: Int = 5): List<MedicineScheduleDomainModel>
+
+}
