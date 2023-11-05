28d1e63315021a9482dee725092daf1a1c74bb7c
Vojtěch Voleman <vojtech.voleman@tul.cz>
2023-11-05 20:13:53 +0100

    BPTULPHR-50 Add GroupMedicineScheduleUseCase with unit tests

--- featureMedicine/src/main/java/cz/vvoleman/phr/featureMedicine/domain/model/timeline/GroupedMedicineSchedulesDomainModel.kt
+++
@@ -1,8 +0,0 @@
-package cz.vvoleman.phr.featureMedicine.domain.model.timeline
-
-import cz.vvoleman.phr.featureMedicine.domain.model.schedule.MedicineScheduleDomainModel
-
-data class GroupedMedicineSchedulesDomainModel (
-    val groupLabel: String,
-    val schedules: List<MedicineScheduleDomainModel>
-)
