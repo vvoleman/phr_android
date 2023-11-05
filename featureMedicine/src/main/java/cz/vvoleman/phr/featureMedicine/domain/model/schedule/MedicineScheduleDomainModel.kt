d8154f83e66ecd751d0bf95914c7cdd0aa5fe99e
Vojtěch Voleman <vojtech.voleman@tul.cz>
2023-11-05 20:13:36 +0100

    BPTULPHR-50 Add GroupMedicineScheduleUseCase with unit tests

--- featureMedicine/src/main/java/cz/vvoleman/phr/featureMedicine/domain/model/schedule/MedicineScheduleDomainModel.kt
+++ featureMedicine/src/main/java/cz/vvoleman/phr/featureMedicine/domain/model/schedule/MedicineScheduleDomainModel.kt
@@ -1,9 +1,12 @@
 package cz.vvoleman.phr.featureMedicine.domain.model.schedule
 
+import android.os.Parcelable
 import cz.vvoleman.phr.common.domain.model.PatientDomainModel
 import cz.vvoleman.phr.featureMedicine.domain.model.medicine.MedicineDomainModel
+import kotlinx.parcelize.Parcelize
 import java.time.LocalDateTime
 
+@Parcelize
 data class MedicineScheduleDomainModel(
     val id: String,
     val patient: PatientDomainModel,
@@ -11,4 +14,4 @@ data class MedicineScheduleDomainModel(
     val schedules: List<ScheduleItemDomainModel>,
     val createdAt: LocalDateTime,
     val isAlarmEnabled: Boolean,
-)
+) : Parcelable
