dce0c980149337df47a7eb95297c9cee01cfe519
Vojtěch Voleman <vojtech.voleman@tul.cz>
2023-11-12 15:15:22 +0100

    BPTULPHR-50 Fix fragment crashing on state change

--- featureMedicine/src/main/java/cz/vvoleman/phr/featureMedicine/ui/model/list/schedule/MedicineScheduleUiModel.kt
+++ featureMedicine/src/main/java/cz/vvoleman/phr/featureMedicine/ui/model/list/schedule/MedicineScheduleUiModel.kt
@@ -1,11 +1,14 @@
 package cz.vvoleman.phr.featureMedicine.ui.model.list.schedule
 
+import android.os.Parcelable
 import cz.vvoleman.phr.common.ui.model.PatientUiModel
 import cz.vvoleman.phr.featureMedicine.ui.model.list.MedicineUiModel
+import kotlinx.parcelize.Parcelize
 import java.time.DayOfWeek
 import java.time.LocalDateTime
 import java.time.LocalTime
 
+@Parcelize
 data class MedicineScheduleUiModel(
     val id: String,
     val patient: PatientUiModel,
@@ -13,7 +16,7 @@ data class MedicineScheduleUiModel(
     val schedules: List<ScheduleItemUiModel>,
     val createdAt: LocalDateTime,
     val isAlarmEnabled: Boolean,
-) {
+) : Parcelable {
 
     fun getTimes(): List<LocalTime> {
         return schedules.map { it.time }.toSet().sorted()
