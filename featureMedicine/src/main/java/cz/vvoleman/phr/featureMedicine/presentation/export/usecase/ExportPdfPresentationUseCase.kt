9c84194125355fd1d4907085ef3cbef89414d7e8
Vojtěch Voleman <vojtech.voleman@tul.cz>
2023-11-18 11:58:56 +0100

    Satisfy coding standards

--- featureMedicine/src/main/java/cz/vvoleman/phr/featureMedicine/presentation/export/usecase/ExportPdfPresentationUseCase.kt
+++
@@ -1,9 +0,0 @@
-package cz.vvoleman.phr.featureMedicine.presentation.export.usecase
-
-import cz.vvoleman.phr.featureMedicine.presentation.export.model.ExportMedicineSchedulePresentationModel
-
-interface ExportPdfPresentationUseCase {
-
-    suspend fun execute(exportData: List<ExportMedicineSchedulePresentationModel>): Boolean
-
-}
