853f1fb5e627a8034cd9be2eadfca0067b06fd5f
Vojtěch Voleman <vojtech.voleman@tul.cz>
2024-01-15 22:29:17 +0100

    Add changes

---
+++ featureMeasurement/src/main/java/cz/vvoleman/featureMeasurement/presentation/model/addEdit/AddEditMeasurementViewState.kt
@@ -0,0 +1,7 @@
+package cz.vvoleman.featureMeasurement.presentation.model.addEdit
+
+import cz.vvoleman.phr.common.presentation.model.patient.PatientPresentationModel
+
+data class AddEditMeasurementViewState(
+    val patient: PatientPresentationModel,
+)
