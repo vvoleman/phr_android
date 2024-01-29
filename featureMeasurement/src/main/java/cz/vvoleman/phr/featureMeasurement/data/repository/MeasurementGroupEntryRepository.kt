9da4b4fb1a251587b7c89fa96ecc8fa645aec706
Vojtěch Voleman <vojtech.voleman@tul.cz>
2024-01-29 14:09:59 +0100

    Add addEditEntry

--- featureMeasurement/src/main/java/cz/vvoleman/phr/featureMeasurement/data/repository/MeasurementGroupEntryRepository.kt
+++ featureMeasurement/src/main/java/cz/vvoleman/phr/featureMeasurement/data/repository/MeasurementGroupEntryRepository.kt
@@ -1,19 +1,22 @@
 package cz.vvoleman.phr.featureMeasurement.data.repository
 
 import cz.vvoleman.phr.featureMeasurement.data.datasource.room.MeasurementGroupEntryDao
+import cz.vvoleman.phr.featureMeasurement.data.datasource.room.MeasurementGroupEntryDataSourceModel
 import cz.vvoleman.phr.featureMeasurement.data.datasource.room.mapper.MeasurementGroupEntryDataSourceModelToDataMapper
 import cz.vvoleman.phr.featureMeasurement.data.mapper.core.MeasurementGroupEntryDataModelToDomainMapper
+import cz.vvoleman.phr.featureMeasurement.domain.model.addEditEntry.SaveMeasurementGroupEntryDomainModel
 import cz.vvoleman.phr.featureMeasurement.domain.model.core.MeasurementGroupEntryDomainModel
 import cz.vvoleman.phr.featureMeasurement.domain.repository.DeleteEntriesByMeasurementGroupRepository
 import cz.vvoleman.phr.featureMeasurement.domain.repository.DeleteEntryRepository
 import cz.vvoleman.phr.featureMeasurement.domain.repository.GetEntryByIdRepository
+import cz.vvoleman.phr.featureMeasurement.domain.repository.SaveEntryRepository
 import kotlinx.coroutines.flow.firstOrNull
 
 class MeasurementGroupEntryRepository(
     private val entryDao: MeasurementGroupEntryDao,
     private val entryDataSourceMapper: MeasurementGroupEntryDataSourceModelToDataMapper,
     private val entryDataMapper: MeasurementGroupEntryDataModelToDomainMapper,
-) : GetEntryByIdRepository, DeleteEntriesByMeasurementGroupRepository, DeleteEntryRepository {
+) : GetEntryByIdRepository, DeleteEntriesByMeasurementGroupRepository, DeleteEntryRepository, SaveEntryRepository {
 
     override suspend fun getEntryById(id: String): MeasurementGroupEntryDomainModel? {
         return entryDao.getById(id).firstOrNull()
@@ -27,5 +30,17 @@ class MeasurementGroupEntryRepository(
 
     override suspend fun deleteEntry(entryId: String) {
         entryDao.delete(entryId)
+    }
+
+    override suspend fun saveEntry(model: SaveMeasurementGroupEntryDomainModel): String {
+        val entry = MeasurementGroupEntryDataSourceModel(
+            id = model.id?.toInt(),
+            createdAt = model.createdAt,
+            measurementGroupId = model.measurementGroupId.toInt(),
+            values = model.values,
+            scheduleItemId = model.scheduleItemId?.toInt()
+        )
+
+        return entryDao.insert(entry).toString()
     }
 }
