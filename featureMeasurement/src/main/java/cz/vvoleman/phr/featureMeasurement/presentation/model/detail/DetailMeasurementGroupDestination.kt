b1e97dc0a29758f3221f38de5175b28cebc6346d
Vojtěch Voleman <vojtech.voleman@tul.cz>
2024-01-29 14:52:48 +0100

    Add base files for DetailMeasurementGroup section

---
+++ featureMeasurement/src/main/java/cz/vvoleman/phr/featureMeasurement/presentation/model/detail/DetailMeasurementGroupDestination.kt
@@ -0,0 +1,7 @@
+package cz.vvoleman.phr.featureMeasurement.presentation.model.detail
+
+import cz.vvoleman.phr.base.presentation.model.PresentationDestination
+
+sealed class DetailMeasurementGroupDestination : PresentationDestination {
+
+}
---
+++ featureMeasurement/src/main/java/cz/vvoleman/phr/featureMeasurement/ui/mapper/detail/destination/DetailMeasurementGroupDestinationUiMapper.kt
@@ -0,0 +1,14 @@
+package cz.vvoleman.phr.featureMeasurement.ui.mapper.detail.destination
+
+import cz.vvoleman.phr.base.presentation.model.PresentationDestination
+import cz.vvoleman.phr.base.presentation.navigation.NavManager
+import cz.vvoleman.phr.base.ui.mapper.DestinationUiMapper
+
+class DetailMeasurementGroupDestinationUiMapper(
+    navManager: NavManager
+) : DestinationUiMapper(navManager) {
+
+    override fun navigate(destination: PresentationDestination) {
+
+    }
+}
