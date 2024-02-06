634eedf17772f441e3706ea2fae3604c256fd843
Vojtěch Voleman <vojtech.voleman@tul.cz>
2024-02-06 22:21:22 +0100

    Add rough list of events using Groupie

---
+++ featureEvent/src/main/java/cz/vvoleman/phr/featureEvent/ui/view/list/groupie/DayContainer.kt
@@ -0,0 +1,43 @@
+package cz.vvoleman.phr.featureEvent.ui.view.list.groupie
+
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.xwray.groupie.GroupieAdapter
+import com.xwray.groupie.viewbinding.BindableItem
+import cz.vvoleman.phr.common.ui.adapter.MarginItemDecoration
+import cz.vvoleman.phr.common.utils.SizingConstants
+import cz.vvoleman.phr.common.utils.getLocalString
+import cz.vvoleman.phr.featureEvent.R
+import cz.vvoleman.phr.featureEvent.databinding.ItemEventDayBinding
+import java.time.LocalDate
+import java.time.format.TextStyle
+
+class DayContainer(
+    private val date: LocalDate,
+    private val items: List<BindableItem<*>>
+) : BindableItem<ItemEventDayBinding>() {
+
+        override fun bind(viewBinding: ItemEventDayBinding, position: Int) {
+            viewBinding.apply {
+                textViewDay.text = date.dayOfMonth.toString()
+                textViewWeekDay.text = date.dayOfWeek.getLocalString(TextStyle.SHORT)
+
+                val groupieAdapter = GroupieAdapter().apply {
+                    addAll(items)
+                }
+
+                recyclerView.apply {
+                    adapter = groupieAdapter
+                    layoutManager = LinearLayoutManager(context)
+                    addItemDecoration(MarginItemDecoration(SizingConstants.MARGIN_SIZE))
+                    setHasFixedSize(true)
+                }
+            }
+        }
+
+        override fun getLayout() = R.layout.item_event_day
+
+        override fun initializeViewBinding(view: View): ItemEventDayBinding {
+            return ItemEventDayBinding.bind(view)
+        }
+}
