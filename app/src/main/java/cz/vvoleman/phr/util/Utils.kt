2f31f9295a9a5ebc8d3015115eb8cbdc264d2b61
Vojtěch Voleman <vojtech.voleman@tul.cz>
2024-02-07 09:47:42 +0100

    Fix backstack pop not rendering

--- app/src/main/java/cz/vvoleman/phr/util/Utils.kt
+++ app/src/main/java/cz/vvoleman/phr/util/Utils.kt
@@ -1,20 +1,6 @@
 package cz.vvoleman.phr.util
 
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collectLatest
-
 val <T> T.exhaustive: T
     get() = this
 
 // lifecycle
-
-fun <T> Fragment.collectLatestLifecycleFlow(
-    flow: Flow<T>,
-    block: (T) -> Unit
-) {
-    viewLifecycleOwner.lifecycleScope.launchWhenStarted {
-        flow.collectLatest(block)
-    }
-}
