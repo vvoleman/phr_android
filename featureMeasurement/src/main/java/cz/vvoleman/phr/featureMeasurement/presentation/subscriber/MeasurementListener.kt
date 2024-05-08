be9fdeb589dc1c79b0b94263b2e533de77f5c085
Vojtěch Voleman <vojtech.voleman@tul.cz>
2024-05-08 11:31:53 +0200

    Add pages to export problem category

--- featureMeasurement/src/main/java/cz/vvoleman/phr/featureMeasurement/presentation/subscriber/MeasurementListener.kt
+++ featureMeasurement/src/main/java/cz/vvoleman/phr/featureMeasurement/presentation/subscriber/MeasurementListener.kt
@@ -2,21 +2,27 @@ package cz.vvoleman.phr.featureMeasurement.presentation.subscriber
 
 import android.net.Uri
 import cz.vvoleman.phr.base.domain.ModuleListener
+import cz.vvoleman.phr.common.presentation.event.problemCategory.ExportProblemCategoryEvent
 import cz.vvoleman.phr.common.presentation.event.problemCategory.GetProblemCategoryDetailSectionEvent
 import cz.vvoleman.phr.common.presentation.eventBus.CommonEventBus
+import cz.vvoleman.phr.common.ui.export.usecase.DocumentPage
 import cz.vvoleman.phr.common.ui.view.problemCategory.detail.groupie.SectionContainer
 import cz.vvoleman.phr.featureMeasurement.domain.model.detail.GetFieldStatsRequest
 import cz.vvoleman.phr.featureMeasurement.domain.repository.GetMeasurementGroupsByProblemCategoryRepository
 import cz.vvoleman.phr.featureMeasurement.domain.usecase.detail.GetFieldStatsUseCase
 import cz.vvoleman.phr.featureMeasurement.presentation.mapper.core.MeasurementGroupPresentationModelToDomainMapper
 import cz.vvoleman.phr.featureMeasurement.presentation.mapper.detail.FieldStatsPresentationModelToDomainMapper
+import cz.vvoleman.phr.featureMeasurement.presentation.mapper.export.ExportDetailParamsPresentationModel
 import cz.vvoleman.phr.featureMeasurement.presentation.model.detail.MeasurementGroupWithStatsPresentationModel
+import cz.vvoleman.phr.featureMeasurement.ui.mapper.detail.EntryInfoUiModelToMeasurementGroupMapper
+import cz.vvoleman.phr.featureMeasurement.ui.view.export.DetailMeasurementGroupPage
 
 class MeasurementListener(
     private val commonEventBus: CommonEventBus,
     private val fieldStatsUseCase: GetFieldStatsUseCase,
     private val getMeasurementsByProblemCategoryRepository: GetMeasurementGroupsByProblemCategoryRepository,
     private val measurementGroupMapper: MeasurementGroupPresentationModelToDomainMapper,
+    private val entryMapper: EntryInfoUiModelToMeasurementGroupMapper,
     private val fieldStatsMapper: FieldStatsPresentationModelToDomainMapper,
     private val problemCategoryDetailProvider: ProblemCategoryDetailProvider,
 ) : ModuleListener() {
@@ -29,6 +35,28 @@ class MeasurementListener(
         commonEventBus.getCategoryDetailSection.addListener(TAG) {
             return@addListener onGetCategoryDetailSection(it)
         }
+
+        commonEventBus.exportProblemCategoryBus.addListener(TAG) {
+            return@addListener onExportProblemCategory(it)
+        }
+    }
+
+    private suspend fun onExportProblemCategory(
+        event: ExportProblemCategoryEvent
+    ): List<DocumentPage> {
+        val groups = getMeasurementsByProblemCategoryRepository
+            .getMeasurementGroupsByProblemCategory(event.problemCategory.id)
+            .map { measurementGroupMapper.toPresentation(it) }
+
+        return groups.map { group ->
+            val entries = entryMapper.toUi(group)
+            val params = ExportDetailParamsPresentationModel(
+                measurementGroup = group,
+                entries = entries
+            )
+
+            splitEntriesToPages(params)
+        }.flatten()
     }
 
     private suspend fun onGetCategoryDetailSection(
@@ -57,6 +85,24 @@ class MeasurementListener(
         }
 
         return listOf(section)
+    }
+
+    private fun splitEntriesToPages(params: ExportDetailParamsPresentationModel): List<DetailMeasurementGroupPage> {
+        val pages = mutableListOf<DetailMeasurementGroupPage>()
+        val entries = params.entries.sortedBy { it.entry.createdAt }
+        val pageCount = entries.size / EXPORT_LIMIT_PER_PAGE + 1
+        for (i in 0 until pageCount) {
+            val start = i * EXPORT_LIMIT_PER_PAGE
+            val end = (i + 1) * EXPORT_LIMIT_PER_PAGE
+            val subEntries = entries.subList(start, end.coerceAtMost(entries.size))
+            val subParams = params.copy(entries = subEntries)
+            pages.add(DetailMeasurementGroupPage(subParams))
+        }
+        return pages
+    }
+
+    companion object {
+        const val EXPORT_LIMIT_PER_PAGE = 11
     }
 
 }
--- featureMedicine/src/main/java/cz/vvoleman/phr/featureMedicine/presentation/subscriber/MedicineListener.kt
+++ featureMedicine/src/main/java/cz/vvoleman/phr/featureMedicine/presentation/subscriber/MedicineListener.kt
@@ -1,19 +1,26 @@
 package cz.vvoleman.phr.featureMedicine.presentation.subscriber
 
+import android.content.Context
 import android.net.Uri
 import cz.vvoleman.phr.base.domain.ModuleListener
+import cz.vvoleman.phr.common.presentation.event.problemCategory.ExportProblemCategoryEvent
 import cz.vvoleman.phr.common.presentation.event.problemCategory.GetProblemCategoryDetailSectionEvent
 import cz.vvoleman.phr.common.presentation.eventBus.CommonEventBus
+import cz.vvoleman.phr.common.ui.export.usecase.DocumentPage
 import cz.vvoleman.phr.common.ui.view.problemCategory.detail.groupie.SectionContainer
 import cz.vvoleman.phr.featureMedicine.domain.repository.GetSchedulesByProblemCategoryRepository
 import cz.vvoleman.phr.featureMedicine.presentation.list.mapper.MedicineSchedulePresentationModelToDomainMapper
 import cz.vvoleman.phr.featureMedicine.presentation.provider.ProblemCategoryDetailProvider
+import cz.vvoleman.phr.featureMedicine.ui.export.view.MedicineExportPage
+import cz.vvoleman.phr.featureMedicine.ui.list.mapper.MedicineScheduleUiModelToPresentationMapper
 
 class MedicineListener(
     private val commonEventBus: CommonEventBus,
     private val getSchedulesByProblemCategoryRepository: GetSchedulesByProblemCategoryRepository,
     private val scheduleMapper: MedicineSchedulePresentationModelToDomainMapper,
+    private val scheduleUiMapper: MedicineScheduleUiModelToPresentationMapper,
     private val problemCategoryDetailProvider: ProblemCategoryDetailProvider,
+    private val context: Context,
 ) : ModuleListener() {
 
     override val TAG = "MedicineListener"
@@ -24,16 +31,21 @@ class MedicineListener(
         commonEventBus.getCategoryDetailSection.addListener(TAG) {
             return@addListener onGetCategoryDetailSection(it)
         }
+
+        commonEventBus.exportProblemCategoryBus.addListener(TAG) {
+            return@addListener onExportProblemCategory(it)
+        }
     }
 
     private suspend fun onGetCategoryDetailSection(
         event: GetProblemCategoryDetailSectionEvent
     ): List<SectionContainer> {
         // Get all records for category
-        val records = getSchedulesByProblemCategoryRepository.getSchedulesByProblemCategory(event.problemCategory.id)
+        val schedules = getSchedulesByProblemCategoryRepository
+            .getSchedulesByProblemCategory(event.problemCategory.id)
             .map { scheduleMapper.toPresentation(it) }
 
-        val section = problemCategoryDetailProvider.getBindingItems(records) { id ->
+        val section = problemCategoryDetailProvider.getBindingItems(schedules) { id ->
             val uri = Uri.parse("phr://list/?medicalRecordId=$id")
 //            navController?.navigate(uri)
         }
@@ -41,5 +53,26 @@ class MedicineListener(
         return listOf(section)
     }
 
+    private suspend fun onExportProblemCategory(
+        event: ExportProblemCategoryEvent
+    ): List<DocumentPage> {
+        val schedules = getSchedulesByProblemCategoryRepository
+            .getSchedulesByProblemCategory(event.problemCategory.id)
+            .map { scheduleMapper.toPresentation(it) }
+
+        val chunks = schedules.chunked(EXPORT_LIMIT_PER_PAGE)
+
+        val pages = mutableListOf<DocumentPage>()
+        for (chunk in chunks) {
+            val page = MedicineExportPage(context, chunk.map { scheduleUiMapper.toUi(it) })
+            pages.add(page)
+        }
+
+        return pages
+    }
+
+    companion object {
+        const val EXPORT_LIMIT_PER_PAGE = 4
+    }
 
 }
