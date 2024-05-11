a635b5c9f4d839231c259d0f9e4a2d6a14d7c13a
Vojtěch Voleman <vojtech.voleman@tul.cz>
2024-05-11 14:21:51 +0200

    Fix issue with event alarms

--- common/src/main/java/cz/vvoleman/phr/common/utils/TimeConstants.kt
+++ common/src/main/java/cz/vvoleman/phr/common/utils/TimeConstants.kt
@@ -3,7 +3,7 @@ package cz.vvoleman.phr.common.utils
 object TimeConstants {
     const val DAYS_IN_WEEK = 7
     const val DEBOUNCE_TIME: Long = 300
-    const val DAY_EPOCH = 864000L
+    const val DAY_EPOCH = 86400L
     const val HOUR_EPOCH = 3600L
     const val MINUTE_EPOCH = 60L
 }
