fd42c2653cd86a4888663545cca8c49bc86742fb
Vojtěch Voleman <vojtech.voleman@tul.cz>
2024-05-13 10:31:38 +0200

    Update medicine notification

--- featureMedicine/src/main/java/cz/vvoleman/phr/featureMedicine/data/alarm/MedicineAlarmReceiver.kt
+++ featureMedicine/src/main/java/cz/vvoleman/phr/featureMedicine/data/alarm/MedicineAlarmReceiver.kt
@@ -3,44 +3,42 @@ package cz.vvoleman.phr.featureMedicine.data.alarm
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Parcelable
 import android.util.Log
 import cz.vvoleman.phr.common.data.alarm.AlarmItem
+import cz.vvoleman.phr.featureMedicine.domain.repository.GetMedicineScheduleByIdRepository
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MedicineAlarmReceiver : BroadcastReceiver() {
 
-    //    @Inject
-//    lateinit var scheduleRepository: ScheduleRepository
+    @Inject
+    lateinit var scheduleRepository: GetMedicineScheduleByIdRepository
 
-    @Suppress("UnusedPrivateProperty")
     override fun onReceive(context: Context?, intent: Intent?) {
-        Log.d(TAG, "onReceive: ${intent?.action}")
         val scope = CoroutineScope(Job() + Dispatchers.Main.immediate)
-        val content = intent?.getParcelableExtra<Parcelable>(AlarmItem.CONTENT_KEY) ?: return
+        val content = intent?.getParcelableExtra<MedicineAlarmContent>(AlarmItem.CONTENT_KEY) ?: return
 
         if (context == null) {
             return
         }
 
         scope.launch {
-//            val medicineSchedule = scheduleRepository.getMedicineScheduleById(testContent.medicineScheduleId)
-//
-//            if (medicineSchedule == null) {
-//                Log.d(TAG, "onReceive: medicineSchedule is null")
-//                return@launch
-//            }
-//
-//            Log.d(TAG, "Received schedule for: ${medicineSchedule.medicine.name}")
-//
-//            Log.d(TAG, "Received: $testContent")
-            val service = CounterNotificationService(context)
-            service.showNotification(++Counter.value)
+            val medicineSchedule = scheduleRepository.getMedicineScheduleById(content.medicineScheduleId)
+
+            if (medicineSchedule == null) {
+                Log.d(TAG, "onReceive: medicineSchedule is null")
+                return@launch
+            }
+            val service = MedicineNotificationService(context)
+            val triggerAt = LocalDateTime.ofEpochSecond(content.triggerAt, 0, ZoneOffset.UTC)
+            service.showNotification(medicineSchedule, triggerAt)
         }
     }
 
--- featureMedicine/src/main/java/cz/vvoleman/phr/featureMedicine/data/alarm/MedicineNotificationService.kt
+++ featureMedicine/src/main/java/cz/vvoleman/phr/featureMedicine/data/alarm/MedicineNotificationService.kt
@@ -9,14 +9,16 @@ import android.net.Uri
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import cz.vvoleman.phr.featureMedicine.R
+import cz.vvoleman.phr.featureMedicine.domain.model.schedule.MedicineScheduleDomainModel
+import java.time.LocalDateTime
 
-class CounterNotificationService(
+class MedicineNotificationService(
     private val context: Context
 ) {
 
     private val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-    fun showNotification(counter: Int) {
+    fun showNotification(medicine: MedicineScheduleDomainModel, timestamp: LocalDateTime) {
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse("phr://medicine/add"))
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
 
@@ -27,27 +29,25 @@ class CounterNotificationService(
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
         )
 
-        val incrementIntent = PendingIntent.getBroadcast(
-            context,
-            2,
-            Intent(context, CounterNotificationReceiver::class.java),
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
-        )
-
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
 
         val notification = NotificationCompat.Builder(context, MedicineAlarmReceiver.CHANNEL_ID)
             .setSmallIcon(R.drawable.baseline_healing_24)
-            .setContentTitle("Upozornění na léky")
-            .setContentText("Je čas vzít si léky ($counter)")
+            .setContentTitle(context.resources.getString(R.string.notification_medicine_title))
+            .setContentText(
+                context.resources.getString(
+                    R.string.notification_medicine_text,
+                    medicine.medicine.name,
+                    timestamp.toLocalTime().toString()
+                )
+            )
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setContentIntent(notificationIntent)
-            .addAction(R.drawable.baseline_healing_24, "Vzít léky", incrementIntent)
             .setSound(defaultSoundUri)
             .setColor(context.getColor(cz.vvoleman.phr.base.R.color.color_primary))
             .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
             .build()
 
-        notificationManager.notify(counter, notification)
+        notificationManager.notify(medicine.id.hashCode(), notification)
     }
 }
